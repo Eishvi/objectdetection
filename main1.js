a8d0aae528f55d829c5d4449cd55684a6e70f2c3
LOL <77565850+Eishvi@users.noreply.github.com>
2022-03-19 14:16:04 +0530

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,25 @@
+img=""
+Status=""
+function preload(){
+img=loadImage("dog_cat.jpg")
+}
+function setup(){
+    canvas=createCanvas(500, 400);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+function draw(){
+    image(img, 0, 0, 500, 400);
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    Status=true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+}
